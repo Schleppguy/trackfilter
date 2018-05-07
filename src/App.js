c6bf427f28d63172788232c7000a0f4593cfce97
Aaron Xavier <aarondxavier@gmail.com>
2018-05-07 10:02:13 -0700

    Added components for Filters. Added example.env file.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'material-design-icons/iconfont/material-icons.css';
 import theme from './assets/react-toolbox/theme.js';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import SC from 'soundcloud';
+import _ from 'lodash';
 
 import AppBar from 'react-toolbox/lib/app_bar/AppBar';
 import Panel from 'react-toolbox/lib/layout/Panel';
@@ -12,6 +13,7 @@ import Layout from 'react-toolbox/lib/layout/Layout';
 import Drawer from 'react-toolbox/lib/drawer/Drawer';
 
 import TrackList from './TrackList';
+import Filters from './Filters';
 import scButton from './assets/btn-connect-sc-l.png';
 
 SC.initialize({
@@ -24,29 +26,45 @@ class App extends Component {
     super(props);
     this.state = {
       tracks: [],
+      followings: [],
+      filteredTracks: []
     }
     this.handleSCClick = this.handleSCClick.bind(this);
+    this.updateFilteredTracks = this.updateFilteredTracks.bind(this);
+  }
+
+  updateFilteredTracks(tracks) {
+    this.setState({ tracks })
   }
 
   handleSCClick() {
     if (process.env.REACT_APP_ENV === 'development') {
       SC.get('/users/7742327/favorites')
       .then(tracks => {
-        this.setState({tracks})
+        this.setState({ tracks, filteredTracks: tracks });
+      });
+
+      SC.get('/users/7742327/followings', {limit: 500})
+      .then(followings => {
+        this.setState({ followings: _.sortBy(followings.collection, ['username']) });
       });
     } else {
       SC.connect().then(() => {      
         return SC.get('/me/activities/tracks/affiliated', {limit: 500, streamable: true});
       }).then(tracks => {
         const collection = tracks.collection.filter(t => t.type === 'track')
-        this.setState({tracks: collection})
+        this.setState({tracks: collection, filteredTracks: collection})
+      }).then(() => {
+        return SC.get('/me/followings', {limit: 500});
+      }).then(followings => {
+        this.setState({ followings: _.sortBy(followings.collection, ['username']) });
       });
     }
   }
   render() {
     let content;
     if (this.state.tracks.length > 0) {
-      content = <TrackList tracks={this.state.tracks} />
+      content = <TrackList tracks={this.state.filteredTracks} />
     } else {
       content = <button onClick={this.handleSCClick} style={{border: 'none', backgroundColor: 'white'}}>
         <img
@@ -68,7 +86,11 @@ class App extends Component {
           </Panel>
           <Drawer active={ true } type={ 'right' } withOverlay={ false } insideTree={ true }>
             <div style={{marginTop: '5em'}}>
-              Filters
+              <Filters 
+                followings={this.state.followings}
+                updateFilteredTracks={this.updateFilteredTracks}
+                tracks={this.state.tracks}
+              />
             </div>
           </Drawer>
         </Layout>
---
+++ src/ArtistListItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ListCheckbox from 'react-toolbox/lib/list/ListCheckbox';
+
+class ArtistListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkboxActive: false
+    };
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+  }
+
+  handleCheckboxChange() {
+    this.props.updateSelectedArtists(this.props.artist)
+    this.setState({checkboxActive: !this.state.checkboxActive})
+  }
+
+  render() {
+    return (
+      <ListCheckbox 
+        checked={this.state.checkboxActive}
+        caption={this.props.artist.username}
+        onChange={this.handleCheckboxChange}
+      />
+    );
+  }
+}
+
+export default ArtistListItem;
