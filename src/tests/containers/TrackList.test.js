c36c85baa415e56aa86c7c47d49e497a9cd1904c
Aaron Xavier <aarondxavier@gmail.com>
2018-05-31 13:58:02 -0700

    Added to isIncluded test

--- src/tests/containers/TrackList.test.js
+++ src/tests/containers/TrackList.test.js
@@ -2,6 +2,10 @@ import { isIncluded } from '../../containers/TrackList';
 
 describe('isIncluded', () => {
   it('should return true', () => {
-    expect(isIncluded('full', 'ul')).toBe(true);
+    expect(isIncluded('FulL', ' UL')).toBe(true);
+  });
+
+  it('should return false', () => {
+    expect(isIncluded('full', 'at')).toBe(false);
   });
 });
