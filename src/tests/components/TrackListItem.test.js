4157f744bc81ae03eafcc42658dadc41c1decb4f
Aaron Xavier <aarondxavier@gmail.com>
2018-05-31 09:28:22 -0700

    Completed TrackListItem test

--- src/tests/components/TrackListItem.test.js
+++ src/tests/components/TrackListItem.test.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import TrackListItem from '../../components/TrackListItem';
-import Card from 'react-toolbox/lib/card/Card';
 import exampleTracks from '../../exampleTracks.json';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<TrackListItem />', () => {
-  it('should throw a TypeError when no track object is provided', () => {
-    expect(() => shallow(<TrackListItem track="foo" />)).toThrow(TypeError);
+  it('should render the track title', () => {
+    const wrapper = render(<TrackListItem track={exampleTracks[1]} />);
+    expect(wrapper.find('h5').text()).toBe('Total Mass Retain');
   });
 
-  it('should not throw a TypeError when a valid track object is provided', () => {
-    expect(() =>
-      shallow(<TrackListItem track={exampleTracks[1]} />)
-    ).not.toThrow(TypeError);
+  it('should render the artist name', () => {
+    const wrapper = render(<TrackListItem track={exampleTracks[1]} />);
+    expect(wrapper.find('p').text()).toBe('kikipopo');
   });
 });
