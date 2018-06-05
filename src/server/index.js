31f528d162b5493c476d9dd598bc98ceb5747b61
Aaron Xavier <aarondxavier@gmail.com>
2018-06-05 13:57:07 -0700

    WIP on server

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// app.use(express.static(path.join(__dirname, '../../public/feed')));
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('/feed', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../build/feed', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+app.listen(9000, () => console.log('Listening on port 9000'));
