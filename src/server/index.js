8672d22fad77c0e99b3bedfc5e2a63a2668b800e
Aaron Xavier <aarondxavier@gmail.com>
2018-06-21 15:09:31 -0700

    remove feed/index.html and send from server

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,14 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('/feed', function(req, res) {
-  res.sendFile(path.join(__dirname, '../../build/feed', 'index.html'));
+  res.format({
+    'text/html': function() {
+      res.send(
+        '<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Connect with SoundCloud</title></head><body onload="window.setTimeout(window.opener.SC.connectCallback, 1)"><b style="text-align: center;">This popup should automatically close in a few seconds</b></body></html>'
+      );
+    }
+  });
+  //res.sendFile(path.join(__dirname, '../../build/feed', 'index.html'));
 });
 
 app.get('/', function(req, res) {
