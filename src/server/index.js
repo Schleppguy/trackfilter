84532d0e6f66cb2e9d17576bc2bb1c67c70e3662
Aaron Xavier <aarondxavier@gmail.com>
2018-06-21 09:56:49 -0700

    debugging auth

--- src/server/index.js
+++ src/server/index.js
@@ -5,11 +5,13 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('/feed', function(req, res) {
+  console.log('in feed');
   console.log(req.params);
   res.sendFile(path.join(__dirname, '../../build/feed', 'index.html'));
 });
 
 app.get('/', function(req, res) {
+  console.log('in /');
   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
