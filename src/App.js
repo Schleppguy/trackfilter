a7b8382776435be833641a7d5793f618a99a5fdf
Aaron Xavier <aarondxavier@gmail.com>
2018-06-26 12:04:15 -0700

    remove react toolbox theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-// import './assets/react-toolbox/theme.css';
-// import 'material-design-icons/iconfont/material-icons.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-// import theme from './assets/react-toolbox/theme.js';
-// import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import PrimaryLayout from './components/PrimaryLayout';
 
 class App extends Component {
